96b6dd66dc8e9a12190714d92717372a4e4270cf
CollinsNugi <emelumbacollins@gmail.com>
2025-06-25 16:58:48 +0100

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
+import { CookiesProvider } from 'react-cookie'; // <-- ADD THIS
+
 import Login from './pages/login/Login.jsx';
 import Home from './pages/Home.jsx';
 import Student from './pages/Student.jsx';
@@ -12,12 +14,10 @@ import Courses from './pages/Courses.jsx';
 import Report from './pages/Report.jsx';
 import Profile from './pages/profile.jsx';
 import RegisterStaff from './pages/RegisterStaff.jsx';
-import { MdAssignment } from 'react-icons/md';
 import Assignment from './pages/Assignment.jsx';
 import Curriculum from './pages/Curriculum.jsx';
-
-
 import Add_courses from './pages/Add_courses.jsx';
+
 import Ai from './uppload/Ai.jsx';
 import Cybersecurity from './uppload/Cybersecurity.jsx';
 import Dbase from './uppload/Dbase.jsx';
@@ -30,39 +30,37 @@ import Pdesign from './uppload/Pdesign.jsx';
 import Pmanagement from './uppload/Pmanagement.jsx';
 import Survey from './uppload/Survey.jsx';
 
-
 createRoot(document.getElementById('root')).render(
-
+  <CookiesProvider>
     <BrowserRouter>
       <Routes>
-        <Route path='/general/dashboard' element={<Home/>}/>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/general/student' element={<Student/>}/>
-        <Route path='/tools/register' element={<Register/>}/>
-        <Route path='/tools/registerstaff' element={<RegisterStaff/>}/>
-        <Route path='/tools/payment' element={<Payment/>}/>
-        <Route path='/student_payment' element={<Student_payment/>}/>
-        <Route path='/general/staff' element={<Staff/>}/>
-        <Route path='/general/courses' element={<Courses/>}/>
-        <Route path='/tools/report' element={<Report/>}/>
-        <Route path='/profile/profile' element={<Profile/>}/>
-        <Route path='/tools/assignment' element={<Assignment/>}/>
-        <Route path='/general/curriculum' element={<Curriculum/>}/>
-       
-        <Route path='/tools/add_courses' element={<Add_courses/>}/>
-        <Route path='/ai' element={<Ai/>}/>
-        <Route path='/cybersecurity' element={<Cybersecurity/>}/>
-        <Route path='/dbase' element={<Dbase/>}/>
-        <Route path='/devops' element={<Devops/>}/>
-        <Route path='/dmarketing' element={<Dmarketing/>}/>
-        <Route path='/dscience' element={<Dscience/>}/>
-        <Route path='/fullstack' element={<FullStack/>}/>
-        <Route path='/mobileapp' element={<MobileApp/>}/>
-        <Route path='/pdesign' element={<Pdesign/>}/>
-        <Route path='/pmanagement' element={<Pmanagement/>}/>
-        <Route path='/survey' element={<Survey/>}/>
-        <Route path='/tools/student_payment' element={<Student_payment/>}/>
+        <Route path='/general/dashboard' element={<Home />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/general/student' element={<Student />} />
+        <Route path='/tools/register' element={<Register />} />
+        <Route path='/tools/registerstaff' element={<RegisterStaff />} />
+        <Route path='/tools/payment' element={<Payment />} />
+        <Route path='/student_payment' element={<Student_payment />} />
+        <Route path='/general/staff' element={<Staff />} />
+        <Route path='/general/courses' element={<Courses />} />
+        <Route path='/tools/report' element={<Report />} />
+        <Route path='/profile/profile' element={<Profile />} />
+        <Route path='/tools/assignment' element={<Assignment />} />
+        <Route path='/general/curriculum' element={<Curriculum />} />
+        <Route path='/tools/add_courses' element={<Add_courses />} />
+        <Route path='/ai' element={<Ai />} />
+        <Route path='/cybersecurity' element={<Cybersecurity />} />
+        <Route path='/dbase' element={<Dbase />} />
+        <Route path='/devops' element={<Devops />} />
+        <Route path='/dmarketing' element={<Dmarketing />} />
+        <Route path='/dscience' element={<Dscience />} />
+        <Route path='/fullstack' element={<FullStack />} />
+        <Route path='/mobileapp' element={<MobileApp />} />
+        <Route path='/pdesign' element={<Pdesign />} />
+        <Route path='/pmanagement' element={<Pmanagement />} />
+        <Route path='/survey' element={<Survey />} />
+        <Route path='/tools/student_payment' element={<Student_payment />} />
       </Routes>
     </BrowserRouter>
-
+  </CookiesProvider>
 );

eab2e2dda0d52fb144aea96a5c08f3f91e16a8e2
jacinta2005 <jacintabisong2021@gmail.com>
2025-06-30 16:27:52 +0100

    push

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://nugi-innovation.onrender.com"
 // const BASE_URL = "http://192.168.0.121:3001"
-const BASE_URL = "/api"
+const BASE_URL = "https://nugi-inn.onrender.com"
 
 export default BASE_URL

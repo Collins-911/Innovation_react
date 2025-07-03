97294c45ca44106b48002328d5e08e62c4d31fbb
jacinta2005 <jacintabisong2021@gmail.com>
2025-07-03 17:11:18 +0100

    push

--- src/utils/authService.js
+++ src/utils/authService.js
@@ -1,46 +1,47 @@
 // Return only user
-export function getUser() {
+export function getUser() {   // fetches the saved user object
   const data = JSON.parse(localStorage.getItem("user"));
   if (!data?.user) return null;
 
-  if (data.user.rol && !data.user.role) {
+  if (data.user.rol && !data.user.role) {   // normalise inconsistent API response
     data.user.role = data.user.rol;
     delete data.user.rol;
   }
 
-  return data.user;
+  return data.user;    // returns the user object 
 }
 
 // Return only token
-export function getToken() {
-  const data = JSON.parse(localStorage.getItem("user"));
-  return data?.token || null;
+export function getToken() {   // fetches the saved authentication token
+  const data = JSON.parse(localStorage.getItem("user"));  // retrieves and parses user from local storage
+  return data?.token || null;  // returns the token if it exists  otherwise returns null
 }
 
 // Return both if needed
-export function getAuthData() {
-  const data = JSON.parse(localStorage.getItem("user"));
-  if (!data?.user || !data?.token) return null;
+export function getAuthData() {    //  fetches both user and token.
+  const data = JSON.parse(localStorage.getItem("user"));   //  retrieves and parses user from local storage 
+  if (!data?.user || !data?.token) return null;    // if either user or token is missing, returned null
 
-  if (data.user.rol && !data.user.role) {
+  if (data.user.rol && !data.user.role) {   // normalise inconsistent API response
     data.user.role = data.user.rol;
     delete data.user.rol;
   }
 
   return {
-    user: data.user,
+    user: data.user,  // returns an object containing both user and token
     token: data.token
   };
 }
 
+// returns the boolean authentication status 
 export function isAuthenticated() {
   const token = getToken();
-  const isAuth = !!token;
+  const isAuth = !!token;   // converts the token value into a boolean
   console.log("Authenticated:", isAuth); // 🔍 Will show true or false in console
   return isAuth;
 }
 
-
+// removes the user data ie logs out
 export function clearUser() {
   localStorage.removeItem("user");
 }
